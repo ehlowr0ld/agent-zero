cfc1f21ecd8b783b724da4c00b08dd06952db3ec
Rafael Uzarowski <uzarowski.rafael@proton.me>
2025-03-10 14:09:44 +0100

    fix: prevent agent from responding beyond the first valid response json object

--- python/extensions/message_loop_end/_95_reset_agent_data.py
+++ python/extensions/message_loop_end/_95_reset_agent_data.py
@@ -7,3 +7,4 @@ class ResetAgentData(Extension):
         self.agent.set_data('thinking_topic', "...")
         self.agent.set_data('response_ttft', '00:00:00')
         self.agent.set_data('response_duration', '00:00:00')
+        self.agent.set_data('agent_responded', False)
